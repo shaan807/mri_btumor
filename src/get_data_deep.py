92596ac9c5e383dbf59863fb7d2d4875ca5fab2f
shaan807 <m.shanawaz807@gmail.com>
2024-03-16 16:03:39 +0530

    get_data_added

---
+++ src/get_data_deep.py
@@ -0,0 +1,25 @@
+from distutils.command.config import config
+
+import os
+import shutil
+import random
+import yaml
+import argparse
+import numpy as np
+import pandas as pd
+
+def get_data_deep(config_file):
+    config=read_params_deep(config_file)
+    return config
+
+def read_params_deep(config_file):
+    with open(config_file) as conf:
+        config=yaml.safe_load(conf)
+    return config
+
+
+if __name__=="__main__":
+    args = argparse.ArguementParser()
+    args.add_arguement("--config",default="deep_params.yaml")
+    parsed_args=args.parse_args()
+    data = get_data_deep(config_file=parsed_args.config)
